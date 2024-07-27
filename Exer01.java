76a8d0d780d56ee2806d6b12055ad1bdb7af5781
Thaynah <80104674+thaynahx@users.noreply.github.com>
2024-07-26 22:45:10 -0300

    Update Exer01.java

--- Exer01.java
+++ Exer01.java
@@ -1,10 +1,12 @@
+/* Faça um Programa que mostre a mensagem "Alo mundo" na tela. */
+
 package com.thaynah.coursejava.aula13;
 
 public class Exer01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Ol� mundo!");
+		System.out.println("Olá mundo!");
 		
 		
 		
