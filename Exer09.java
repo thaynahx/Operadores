209c69bb381303e00c4117866983dcb30f751f67
Thaynah <80104674+thaynahx@users.noreply.github.com>
2024-07-26 22:49:43 -0300

    Update Exer09.java

--- Exer09.java
+++ Exer09.java
@@ -1,3 +1,7 @@
+/* Faça um Programa que peça a temperatura em graus Farenheit,
+transforme e mostre a temperatura em graus Celsius.
+o C = (5 * (F-32) / 9). */
+
 package com.thaynah.coursejava.aula13;
 
 import java.util.Scanner;
@@ -13,7 +17,7 @@ public class Exer09 {
 		
 		double c = (5 * (f-32) / 9);
 		
-		System.out.println("A temperatura em farenheit " + f + "\n� igual a temperatura em Celcius " + c + "�."  );
+		System.out.println("A temperatura em farenheit " + f + "\né igual a temperatura em Celcius " + c + "º."  );
 		
 		
 	}
