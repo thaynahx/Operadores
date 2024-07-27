b3002444f16fbca302fa175622d2dd0860e9c21a
Thaynah <80104674+thaynahx@users.noreply.github.com>
2024-07-26 23:02:27 -0300

    Update Exer15.java

--- Exer15.java
+++ Exer15.java
@@ -1,3 +1,22 @@
+/* Faça um Programa que pergunte quanto você ganha por hora e o
+número de horas trabalhadas no mês. Calcule e mostre o total do seu
+salário no referido mês, sabendo-se que são descontados 11% para o
+Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um
+programa que nos dê:
+
+. salário bruto.
+a. quanto pagou ao INSS.
+b. quanto pagou ao sindicato.
+c. o salário líquido.
+d. calcule os descontos e o salário líquido, conforme a tabela
+abaixo:
+
++ Salário Bruto : R$ - IR (11%) : R$ - INSS
+(8%) : R$ - Sindicato ( 5%) : R$ = Salário
+Liquido : R$
+
+Obs.: Salário Bruto - Descontos = Salário Líquido.*/
+
 package com.thaynah.coursejava.aula13;
 
 import java.util.Scanner;
@@ -10,7 +29,7 @@ public class Exer15 {
 		
 		System.out.println("Entre com o valor/hora: ");
 		double valorHora = ler.nextDouble();
-		System.out.println("Entre com a quantidade de horas trabalhadas no m�s: ");
+		System.out.println("Entre com a quantidade de horas trabalhadas no mês: ");
 		double horas = ler.nextDouble();
 		
 		double salarioBruto = valorHora*horas;
@@ -22,12 +41,12 @@ public class Exer15 {
 		double descontos = IR + INSS + sindicato;
 		double salarioLiquido = salarioBruto - descontos;
 		
-		System.out.println("Sal�rio Bruto: " + salarioBruto);
+		System.out.println("Salário Bruto: " + salarioBruto);
 		System.out.println("Imposto de renda:" + IR);
 		System.out.println("INSS:" + INSS);
 		System.out.println("Sindicato: " + sindicato);
 		System.out.println("Total de descontos: " + descontos);
-		System.out.println("Sal�rio L�quido: " + salarioLiquido);
+		System.out.println("Salário Líquido: " + salarioLiquido);
 		
 		
 		
